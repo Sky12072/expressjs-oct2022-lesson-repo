d65b5c1f10ccc470c24d937b9732dc9b68d3db27
Sky12072 <12072@coderacademy.edu.au>
2022-10-21 16:03:15 +1100

    add seperate lines on server.js

--- src/server.js
+++ src/server.js
@@ -73,7 +73,10 @@ if (process.env.NODE_ENV != "test") {
         console.log("Database connected, wohoo!!")
     }).catch(error => {
         // if database connection failed, log the error
-        console.log (`Some error occured, it was: ${error}`)
+        console.log(`
+        Some error occured, it was: 
+        ${error}
+        `)
     });
 }
 
