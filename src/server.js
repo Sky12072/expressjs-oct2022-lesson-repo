7cac9c395f765fb5386883dbf054ba949a60b796
Sky12072 <12072@coderacademy.edu.au>
2022-10-21 15:58:29 +1100

    re-add tim's server.js

--- src/server.js
+++ src/server.js
@@ -66,19 +66,16 @@ const {databaseConnector} = require('./database');
 
 // if it's a not a test environment
 if (process.env.NODE_ENV != "test") {
-	const DATABASE_URI = process.env.DATABASE_URI || "mongodb://localhost:27017/ExpressLessonOctLocal";
-	databaseConnector(DATABASE_URI).then(() => {
-		// if database connection succeeded, log a nice success message 
-		console.log("Database connected, woohoo!");
-	}).catch(error => {
-		// if database connection failed, log the error
-		console.log(`
-		Some error occured, it was: 
-		${error}
-		`)
-	});
-} 
-//what is 27017 coming from, and why when I changed it, it'll fail ? 27017 is the default mongoose url maybe
+    const DATABASE_URI = process.env.DATABASE_URI || "mongodb://localhost:27017/ExpressLessonOctLocal" 
+    //what is 27017 coming from, and why when I changed it, it'll fail ? 27017 is the default mongoose url maybe
+    databaseConnector(DATABASE_URI).then(() => {
+        // if database connection succeeded, log a nice success message
+        console.log("Database connected, wohoo!!")
+    }).catch(error => {
+        // if database connection failed, log the error
+        console.log (`Some error occured, it was: ${error}`)
+    });
+}
 
 //------------------------------------------------
 // Config Above
